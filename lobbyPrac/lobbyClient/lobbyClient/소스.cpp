e5c20924f8cbc75622989453b1a7ae29890a4e82
taeshs <taejustar@gmail.com>
2024-01-15 16:42:15 +0900

    echo server loop

--- lobbyPrac/lobbyClient/lobbyClient/소스.cpp
+++ lobbyPrac/lobbyClient/lobbyClient/소스.cpp
@@ -5,23 +5,39 @@
 
 int main() {
 	WSADATA wsa;
-	WSAStartup(MAKEWORD(2, 2), &wsa);
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) { // 0 OR Errorcodes
+		std::cout << "socket start error" << std::endl;
+		return 0;
+	}
 
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET) {
+		std::cout << "socket init error" << std::endl;
+		return 0;
+	}
 	SOCKADDR_IN addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(8888);
 	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	connect(sock, (SOCKADDR*)&addr, sizeof(addr));
+	
+	if (connect(sock, (SOCKADDR*)&addr, sizeof(addr))) { // 0 OR SOCKET_ERROR ( WSAGetLastError() )
+		std::cout << "socket connection error" << std::endl;
+		return 0;
+	}
+	std::cout << "server connected." << std::endl;
 
 	char buf[128] = {};
-	recv(sock, buf, sizeof(buf), 0);
 
-	std::cout << buf << std::endl;
-
-	memcpy(buf, "hi from client", 15);
-
-	send(sock, buf, sizeof(buf), 0);
+	while(true) {
+		std::cin >> buf;
+		if (strcmp(buf, "EXIT") == 0) {
+			break;
+		}
+		send(sock, buf, sizeof(buf), 0);
+		recv(sock, buf, sizeof(buf), 0);
+		std::cout << buf << std::endl;
+		memset(buf, 0, sizeof(buf));
+	}
 
 	closesocket(sock);
 
--- lobbyPrac/lobbyServer/lobbyServer/소스.cpp
+++ lobbyPrac/lobbyServer/lobbyServer/소스.cpp
@@ -5,30 +5,44 @@
 
 int main() {
 	WSADATA wsa = {0};
-	WSAStartup(MAKEWORD(2, 2), &wsa);
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) { // 0 OR Errorcodes
+		std::cout << "socket start error" << std::endl;
+		return 0;
+	}
 
 	SOCKET lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (lsock == INVALID_SOCKET) {
+		std::cout << "socket init error" << std::endl;
+		return 0;
+	}
 
 	SOCKADDR_IN addr = { 0 };
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(8888);
 	addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-	bind(lsock, (SOCKADDR*)&addr, sizeof(addr));
+	if (bind(lsock, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR) { // 0 OR SOCKET_ERROR ( WSAGetLastError() )
+		std::cout << "socket bind error" << std::endl;
+		return 0;
+	}
 
-	listen(lsock, SOMAXCONN);
+	if(listen(lsock, SOMAXCONN)){ // 0 OR SOCKET_ERROR ( WSAGetLastError() )
+		std::cout << "socket listen error" << std::endl;
+		return 0;
+	}
 
 	SOCKADDR_IN caddr = { 0 };
 	int csize = sizeof(caddr);
-	SOCKET csock = accept(lsock, (SOCKADDR*)&caddr, &csize);
-
-	char buf[128] = "hi from server";
-	send(csock, buf, sizeof(buf), 0);
-
-	memset(buf, 0, sizeof(buf));
-
-	recv(csock, buf, sizeof(buf), 0);
-
-	std::cout << buf << std::endl;
+	SOCKET csock;
+	char buf[128];
+	while ((csock = accept(lsock, (SOCKADDR*)&caddr, &csize))) {
+		std::cout << "new client connected." << std::endl;
+		while( recv(csock, buf, sizeof(buf), 0) ) {
+			std::cout << buf << std::endl;
+			send(csock, buf, sizeof(buf), 0);
+			memset(buf, 0, sizeof(buf));
+		}
+		std::cout << "client disconnected." << std::endl;
+	}	
 
 	closesocket(csock);
 	closesocket(lsock);
